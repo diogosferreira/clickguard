18198f64c0a4ba2f70989c7ebf8789f8f5b815a5
diogosferreira <diogosferreira@users.noreply.github.com>
2025-07-15 15:53:04 +0100

    sent

--- public/swiper/solves.js
+++ public/swiper/solves.js
@@ -2,40 +2,51 @@ export function swiperSolves() {
     const element = document.querySelector(".swiper-how-solves");
     if (!element) return;
 
-    const swiper = new Swiper(".swiper-how-solves", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        initialSlide: 2,
-        slidesPerView: "auto",
-        //slidesPerView: 5,
-        loop: false, // optional
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 50,
-            depth: 100, // increase from 150 to 200 or more
-            modifier: 3, // slightly increase to amplify depth effect
-            slideShadows: false,
-        },
-        pagination: {
-            el: ".swiper-pagination-solves",
-            clickable: true,
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '"></span>';
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const swiper = new Swiper(".swiper-how-solves", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            initialSlide: 2,
+            slidesPerView: "auto",
+            loop: false,
+            watchSlidesProgress: true,
+
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 50,
+                depth: 100,
+                modifier: 3,
+                slideShadows: false,
             },
-        },
-    });
+
+            pagination: {
+                el: ".swiper-pagination-solves",
+                clickable: true,
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '"></span>';
+                },
+            },
+
+            on: {
+                slideChange: function () {
+                    swiper.pagination.update();
+                },
+            },
+        });
 
 
 
-    $("[data-slide]").on("click", function (e) {
-        e.preventDefault();
-        const direction = $(this).attr("data-slide");
-        if (direction === "next") {
-            swiper.slideNext();
-        } else if (direction === "prev") {
-            swiper.slidePrev();
-        }
+        $("[data-slide]").on("click", function (e) {
+            e.preventDefault();
+            const direction = $(this).attr("data-slide");
+            if (direction === "next") {
+                swiper.slideNext();
+            } else if (direction === "prev") {
+                swiper.slidePrev();
+            }
+        });
     });
 
 }
